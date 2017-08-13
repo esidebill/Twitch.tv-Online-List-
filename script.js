a470b1c0b9ae913b3ca1c5c011e944ec5f1ecba2
Bill <billwalsh006@gmail.com>
2017-08-12 22:16:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,27 @@ $(document).ready(function() {
   function add_streamer() {
     var user_id = $("#user_input").val();
     var twitch_api_user =
-      "https://wind-bow.glitch.me/twitch-api/users/" + user_id;
+        "https://wind-bow.glitch.me/twitch-api/users/" + user_id;
     var twitch_api_stream =
-      "https://wind-bow.glitch.me/twitch-api/streams/" + user_id;
+        "https://wind-bow.glitch.me/twitch-api/streams/" + user_id;
     $.getJSON(twitch_api_stream, function(data) {
       if (data.stream === null) {
         $.getJSON(twitch_api_user, function(data) {
-          $("#display").append(
-            "<div class='jumbotron stream_offline'>" + "<div class='row'>" + "<div class='col-sm-4'>" +
+          if (data.error === "Not Found") {
+            $("#display").append(
+              "<div class='jumbotron'>" +
+              "There's no streamer named " +
+              user_id +
+              "<br><button class='btn btn-secondary btn-sm remove2' style='margin-top:15px;'>Remove streamer</button></div>"
+            );
+            $(".remove2").click(function() {
+              $(this).parent().remove();
+            });
+          } else {
+            $("#display").append(
+              "<div class='jumbotron stream_offline'>" +
+              "<div class='row'>" +
+              "<div class='col-sm-4'>" +
               "<a href='https://www.twitch.tv/" +
               data.name +
               "' target='_blank'>" +
@@ -23,7 +36,8 @@ $(document).ready(function() {
               data.display_name +
               "</strong>" +
               "<br>Stream <span id='offline_text'>offline</span></p></div><br><button class='btn btn-secondary btn-sm remove'>Remove streamer</button></div></div>"
-          );
+            );
+          }
           $(".remove").click(function() {
             $(this).parent().parent().remove();
           });
@@ -31,21 +45,23 @@ $(document).ready(function() {
       } else {
         $.getJSON(twitch_api_stream, function(data) {
           $("#display").append(
-            "<div class='jumbotron stream_online'>" + "<div class='row'>" + "<div class='col-sm-4'>" +
-              "<a href='https://www.twitch.tv/" +
-              data.stream.channel.name +
-              "'target='_blank'>" +
-              "<img class='img-fluid rounded-circle userLogo' src='" +
-              data.stream.channel.logo +
-              "'/></a></div>" +
-              "<br>" +
-              "<div class='col-sm-8'><p class='streamerInfo'><strong>" +
-              data.stream.channel.display_name +
-              "</strong>" +
-              "<br>Stream <span id='online_text'>online</span>" +
-              "<br>Playing: " +
-              data.stream.game +
-              "</p></div><br><button class='btn btn-secondary btn-sm remove'>Remove streamer</button></div></div>"
+            "<div class='jumbotron stream_online'>" +
+            "<div class='row'>" +
+            "<div class='col-sm-4'>" +
+            "<a href='https://www.twitch.tv/" +
+            data.stream.channel.name +
+            "'target='_blank'>" +
+            "<img class='img-fluid rounded-circle userLogo' src='" +
+            data.stream.channel.logo +
+            "'/></a></div>" +
+            "<br>" +
+            "<div class='col-sm-8'><p class='streamerInfo'><strong>" +
+            data.stream.channel.display_name +
+            "</strong>" +
+            "<br>Stream <span id='online_text'>online</span>" +
+            "<br>Playing: " +
+            data.stream.game +
+            "</p></div><br><button class='btn btn-secondary btn-sm remove'>Remove streamer</button></div></div>"
           );
           $(".remove").click(function() {
             $(this).parent().parent().remove();
@@ -71,7 +87,5 @@ $(document).ready(function() {
   $("#player").on("click", function() {
     add_streamer();
   });
-  $(".remove").on("click", function() {
-    $(this).parent().parent().remove();
-  });
 });
+
