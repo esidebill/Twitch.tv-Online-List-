70326ef0bf8bcbb58e356d04d8d70d2418d5ce0a
Bill <billwalsh006@gmail.com>
2017-08-13 20:02:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,45 @@
 $(document).ready(function() {
   $(".dropdown-toggle").dropdown();
 
+  //call function to add a new streamer
   function add_streamer() {
+    //set variables for input bar and APIs
     var user_id = $("#user_input").val();
     var twitch_api_user =
       "https://wind-bow.glitch.me/twitch-api/users/" + user_id;
     var twitch_api_stream =
       "https://wind-bow.glitch.me/twitch-api/streams/" + user_id;
 
+    //gets JSON info from the twitch API
     $.getJSON(twitch_api_stream, function(data) {
+      //if the user entered a streamer that exists an error appears
       if ($("#" + user_id).length == 1) {
-        $("#alert_box").html("<button type='button' id='close_button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button><strong>Whoops!</strong> That streamer's already been added");
-            $("#alert_box").css("display", "block");
-            $("#close_button").on("click", function() {
-    $("#alert_box").css("display", "none");
-              $("#alert_box").html("")
-  });
+        $("#alert_box").html(
+          "<button type='button' id='close_button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button><strong>Whoops!</strong> That streamer's already been added"
+        );
+        $("#alert_box").css("display", "block");
+        $("#close_button").on("click", function() {
+          $("#alert_box").css("display", "none");
+          $("#alert_box").html("");
+        });
         $("#user_input").val("");
+        //if theres no stream information, the user is offline
       } else if (data.stream === null) {
+        //gets JSON info for the offline user
         $.getJSON(twitch_api_user, function(data) {
+          //if there is no such user an error appears
           if (data.error === "Not Found") {
-            $("#alert_box").html("<button type='button' id='close_button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button><strong>Whoops!</strong> No such streamer");
+            $("#alert_box").html(
+              "<button type='button' id='close_button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button><strong>Whoops!</strong> No such streamer"
+            );
             $("#alert_box").css("display", "block");
             $("#close_button").on("click", function() {
-    $("#alert_box").css("display", "none");
-              $("#alert_box").html("")
-  });
-          
+              $("#alert_box").css("display", "none");
+              $("#alert_box").html("");
+            });
+
             $("#user_input").val("");
+            //if it is a valid ID it displays their information in a new div
           } else {
             $("#user_input").val("");
             $("#display").append(
@@ -52,65 +64,73 @@ $(document).ready(function() {
               )
               .slideDown("slow");
           }
+          //button removes streamer from list
           $(".remove").click(function() {
             $(this).slideUp("slow", function() {
-            $(this).parent().parent().parent().remove();
+              $(this).parent().parent().parent().remove();
             });
           });
         });
       } else {
+        //if user is online, gets stream information
         $.getJSON(twitch_api_stream, function(data) {
+          //adds new div with stream information
           $("#user_input").val("");
-$("#display").append(
-              "<div id='" + user_id + "' style='display:none;'></div>"
-            );
-          $("#" + user_id).append(
-            "<div class='jumbotron stream_online'>" +
-              "<div class='row'>" +
-              "<div class='col-sm-4'>" +
-              "<a href='https://www.twitch.tv/" +
-              data.stream.channel.name +
-              "'target='_blank'>" +
-              "<img class='img-fluid userLogo rounded-circle' alt='' src='" +
-              data.stream.channel.logo +
-              "'/></a></div>" +
-              "<br>" +
-              "<div class='col-sm-8'><p class='streamerInfo'><strong>" +
-              data.stream.channel.display_name +
-              "</strong>" +
-              "<br>Stream <span id='online_text'>online</span>" +
-              "<br>Playing: " +
-              data.stream.game +
-              "</p></div><br><button class='btn btn-secondary btn-sm remove'>Remove streamer</button></div></div>"
-          ).slideDown("slow");
+          $("#display").append(
+            "<div id='" + user_id + "' style='display:none;'></div>"
+          );
+          $("#" + user_id)
+            .append(
+              "<div class='jumbotron stream_online'>" +
+                "<div class='row'>" +
+                "<div class='col-sm-4'>" +
+                "<a href='https://www.twitch.tv/" +
+                data.stream.channel.name +
+                "'target='_blank'>" +
+                "<img class='img-fluid userLogo rounded-circle' alt='' src='" +
+                data.stream.channel.logo +
+                "'/></a></div>" +
+                "<br>" +
+                "<div class='col-sm-8'><p class='streamerInfo'><strong>" +
+                data.stream.channel.display_name +
+                "</strong>" +
+                "<br>Stream <span id='online_text'>online</span>" +
+                "<br>Playing: " +
+                data.stream.game +
+                "</p></div><br><button class='btn btn-secondary btn-sm remove'>Remove streamer</button></div></div>"
+            )
+            .slideDown("slow");
+          //button to remove streamer
           $(".remove").click(function() {
             $(this).slideUp("slow", function() {
-            $(this).parent().parent().parent().remove();
+              $(this).parent().parent().parent().remove();
             });
           });
         });
       }
     });
   }
-
+  //in sort dropdown to show all streamers
   $("#all").on("click", function() {
     $(".stream_offline").show();
     $(".stream_online").show();
   });
+  //in sort dropdown to only show online
   $("#online").on("click", function() {
     $(".stream_offline").hide();
     $(".stream_online").show();
   });
+  //in sort dorpdown to only show offline
   $("#offline").on("click", function() {
     $(".stream_online").hide();
     $(".stream_offline").show();
   });
+  //clicking Go will run this function
   $("#games").on("click", function() {});
   $("#player").on("click", function() {
     add_streamer();
   });
-   
-
+  //allows enter to use Go button
   $(document).bind("keypress", function(e) {
     if (e.keyCode == 13) {
       $("#player").trigger("click");
